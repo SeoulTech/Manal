427e3b25fcd66ff231b745b603718acc27860148
kolyaak <nikolay.akatyev@gmail.com>
2014-04-30 17:58:11 +0900

    Start applying a view to display sinks.

--- Manal-plugin/src/com/dforensic/plugin/manal/ManalManager.java
+++ Manal-plugin/src/com/dforensic/plugin/manal/ManalManager.java
@@ -2,11 +2,18 @@ package com.dforensic.plugin.manal;
 
 import java.util.List;
 
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
 import soot.jimple.infoflow.InfoflowResults.SinkInfo;
 
 import com.dforensic.plugin.manal.input.flowdroid.FlowDroidExecutor;
 import com.dforensic.plugin.manal.input.flowdroid.FlowDroidExecutor.FlowDroidCallback;
 import com.dforensic.plugin.manal.model.ApiDescriptor;
+import com.dforensic.plugin.manal.views.SuspectListVw;
 
 public class ManalManager {
 
@@ -30,6 +37,12 @@ public class ManalManager {
 	}
 
 	public void displaySinks() {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+        IWorkbenchPage page = win.getActivePage();
+
+	    SuspectListVw view = (SuspectListVw) page.findView(SuspectListVw.ID);
+		/*
 		if (mDiscoveredSinks != null) {
 			for (ApiDescriptor sink : mDiscoveredSinks) {
 				if (sink != null) {
@@ -47,6 +60,7 @@ public class ManalManager {
 		} else {
 			System.err.println("No sinks discovered to display.");
 		}
+		*/
 	}
 
 	public List<ApiDescriptor> getDiscoveredSinks() {
