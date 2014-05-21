7bd386662fd075b465e18e4f579f300f5e33aed0
kolyaak <nikolay.akatyev@gmail.com>
2014-05-21 23:54:06 +0900

    Added android platform input, run FlowDroid with selected properties: apk path.

--- Manal-plugin/src/com/dforensic/plugin/manal/model/ProjectProperties.java
+++ Manal-plugin/src/com/dforensic/plugin/manal/model/ProjectProperties.java
@@ -1,28 +1,57 @@
 package com.dforensic.plugin.manal.model;
 
 public class ProjectProperties {
+	
+	public static final String QUALIFIER = "Manal";
 
-	private static String apkName;
-	private static String prjName;
+	private static final String apkNameKey = "apkNameKey";
+	private static String apkNameVal;
+	
+	private static final String prjNameKey = "prjNameKey";
+	private static String prjNameVal;
+	
+	private static final String androidPathKey = "androidPathKey";
+	private static String androidPathVal;
 	
 	public ProjectProperties()
 	{
 		
 	}
 
-	public static String getProjName() {
-		return prjName;
+	public static String getPrjNameVal() {
+		return prjNameVal;
+	}
+	
+	public static String getPrjNameKey() {
+		return prjNameKey;
 	}
 
-	public static void setProjName(String prjName) {
-		ProjectProperties.prjName = prjName;
+	public static void setPrjNameVal(String prjName) {
+		prjNameVal = prjName;
 	}
 
-	public static String getApkName() {
-		return apkName;
+	public static String getApkNameVal() {
+		return apkNameVal;
+	}
+	
+	public static String getApkNameKey() {
+		return apkNameKey;
 	}
 
-	public static void setApkName(String apkName) {
-		ProjectProperties.apkName = apkName;
+	public static void setApkNameVal(String apkName) {
+		apkNameVal = apkName;
 	}
+	
+	public static String getAndroidPathVal() {
+		return androidPathVal;
+	}
+	
+	public static String getAndroidPathKey() {
+		return androidPathKey;
+	}
+
+	public static void setAndroidPathVal(String androidPath) {
+		androidPathVal = androidPath;
+	}
+	
 }
